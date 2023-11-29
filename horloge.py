8487a77b6d0bd8def45bf467535642264b50d405
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-11-29 12:55:53 +0100

    ajout du job terminé

--- horloge.py
+++ horloge.py
@@ -2,10 +2,13 @@ import time
 
 heure = (23, 59, 55)
 alarme = (23, 59, 58)
+choix_stop = (0, 0, 0)
 
 def afficher_heure(heure):
+
     heure_liste = list(heure)
     heures, minutes, secondes = heure_liste
+
     while True:
         if 0 <= secondes < 60 and 0 <= minutes < 60 and 0 <= heures < 24:
             secondes = (secondes + 1) % 60
@@ -13,10 +16,13 @@ def afficher_heure(heure):
                 minutes = (minutes + 1) % 60
                 if minutes == 0:
                     heures = (heures + 1) % 24
-
+                
         time.sleep(1)
+
         regler_alarme(alarme, (heures, minutes, secondes))
         am_pm(heures, minutes, secondes, format_choisi)
+        stop(heures, minutes, secondes, choix_stop)
+
 format_choisi = input("Choisissez le format (24h ou am/pm): ")
 
 
@@ -26,8 +32,10 @@ def regler_alarme(alarme, heure):
 
 
 def am_pm(heures, minutes, secondes, format_choisi):
+
     if format_choisi.lower() == "24h":
         print(f"{heures:02d}:{minutes:02d}:{secondes:02d}")
+
     elif format_choisi.lower() == "am/pm":
         suffixe = "AM"
         if heures >= 12:
@@ -38,8 +46,24 @@ def am_pm(heures, minutes, secondes, format_choisi):
             heures = 12
 
         print(f'{heures:02d}:{minutes:02d}:{secondes:02d} {suffixe}')
+
     else:
         print("Format non reconnu. Veuillez choisir entre '24h' ou 'am/pm'.")
+
+def stop(heures, minutes, secondes, choix_stop):
+    if choix_stop == (heures, minutes, secondes):
+        pause_reponse = input("L'horloge est suspendue. Voulez-vous continuer ? (oui/non): ")
+        if pause_reponse.lower() == "oui":
+            pass
+        elif pause_reponse.lower() == "non":
+            print("Horloge suspendue. Veuillez écrire 'reprendre' pour continuer.")
+            while True:
+                reprise = input("Attente de reprise : ")
+                if reprise.lower() == "reprendre":
+                    print("Horloge reprise.")
+                    break
+                else:
+                    print("Commande non reconnue.")
 
 
 afficher_heure(heure)
